4d21d90b78c9bc63dd246f3b03ad72722dfbe809
Kaio Soares <kaioantoniosoares@gmail.com>
2022-09-26 22:44:09 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,14 @@
+import NotificationButton from "./components/notificationButton"
+
 function App() {
   return (
+    <>
       <h1>Olá mundo!</h1>
-    )
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
---
+++ frontend/src/components/notificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/images/notificationIcon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
