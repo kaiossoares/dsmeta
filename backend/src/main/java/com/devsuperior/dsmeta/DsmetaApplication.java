a70dc77b907c2e811a7784e19826276f2d445d2d
Kaio Soares <kaioantoniosoares@gmail.com>
2022-09-30 19:12:23 -0300

    Database

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -2,6 +2,8 @@ package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 
 @SpringBootApplication
 public class DsmetaApplication {
