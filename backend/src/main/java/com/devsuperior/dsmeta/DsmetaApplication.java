c2755037c2843fb7bb13d5a5b7af2eee48669357
Kaio Soares <kaioantoniosoares@gmail.com>
2022-10-01 11:54:35 -0300

    Twilio SMS

--- backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
+++ backend/src/main/java/com/devsuperior/dsmeta/DsmetaApplication.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsmeta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,13 +3,11 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,6 +15,8 @@ public class SaleController {
 
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsService smsService;
 
     @GetMapping
     public Page<Sale> findSales(
@@ -24,5 +24,10 @@ public class SaleController {
             @RequestParam(value = "maxDate", defaultValue = "") String maxDate,
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
